857f7ea05f05d2e3be85d2ba702b7e8bc77983ed
Добровольский Леонид <l_dobrovolsky@viyar.com.ua>
2026-04-30 19:03:48 +0300

    task1

--- task1.py
+++ task1.py
@@ -1,6 +1,25 @@
 import os
 import sys
 import os
+import shutil
+
+class File:
+    def __init__(self, folder, fullname):
+        self.folder         = folder
+        self.fullname       = fullname
+        self.path           = folder + "\\" + fullname
+
+        last_dot_index = fullname.rfind('.')
+
+        if last_dot_index != -1:
+            self.app  = fullname[last_dot_index + 1:]
+            self.name = fullname[:last_dot_index]
+        else:
+            self.app  = ""
+            self.name = fullname
+
+    def _print(self):
+        print(f"Folder: {self.folder}    name: {self.name}    app: {self.app}  path: {self.path}")
 
 def normalize(file_name):
     translit_dict = {
@@ -35,28 +54,12 @@ def normalize(file_name):
 
     return normalize_name
 
-class File:
-    def __init__(self, folder, fullname):
-        self.folder   = folder
-        self.fullname = fullname
-
-        last_dot_index = fullname.rfind('.')
-
-        if last_dot_index != -1:
-            self.app = fullname[last_dot_index + 1:]
-            self.name = fullname[:last_dot_index]
-        else:
-            self.app = ""
-            self.name = fullname
-
-    def _print(self):
-        print(f"Folder: {self.folder} name: {self.name} app: {self.app}")
-
 
 def read_file_names_from_folder(folder, files):
 
     for file in os.listdir(folder):
-        if os.path.isdir(file):
+
+        if os.path.isdir(folder + "\\" + file):
             read_file_names_from_folder(folder + "\\" + file, files)
         else:
             files_fullnames = {file_elem.fullname: i for i, file_elem in enumerate(files)}
@@ -79,12 +82,57 @@ def sort_files(folder):
     for file in files:
         file._print()
 
-    image_app = ('JPEG', 'PNG', 'JPG', 'SVG')
-    video_app = ('AVI', 'MP4', 'MOV', 'MKV')
+    images_app     = ('JPEG', 'PNG', 'JPG', 'SVG')
+    videos_app     = ('AVI', 'MP4', 'MOV', 'MKV')
     documents_app = ('DOC', 'DOCX', 'TXT', 'PDF', 'XLSX', 'PPTX')
-    music_app = ('MP3', 'OGG', 'WAV', 'AMR')
-    archive_app = ('ZIP', 'GZ', 'TAR')
+    musics_app     = ('MP3', 'OGG', 'WAV', 'AMR')
+    archives_app   = ('ZIP', 'GZ', 'TAR')
 
+    #create temp folder
+    temp_folder = "." + folder
+    while os.path.exists(temp_folder):
+        temp_folder = "." + temp_folder
+
+    os.mkdir(temp_folder)
+
+    #create app folder
+    os.mkdir(temp_folder + "\\images")
+    os.mkdir(temp_folder + "\\videos")
+    os.mkdir(temp_folder + "\\documents")
+    os.mkdir(temp_folder + "\\musics")
+    os.mkdir(temp_folder + "\\archives")
+
+    #copy files
+    for file in files:
+        normalize_name = normalize(file.name)
+        file.normalize_name = normalize_name
+        folder_app_name = "\\other\\"
+
+        if file.app in images_app:
+            folder_app_name = "\\images\\"
+        elif file.app in videos_app:
+            folder_app_name = "\\videos\\"
+        elif file.app in documents_app:
+            folder_app_name = "\\documents\\"
+        elif file.app in musics_app:
+            folder_app_name = "\\musics\\"
+        elif file.app in archives_app:
+            folder_app_name = "\\archives\\"
+
+        symbol_dot = ""
+        if file.app != "":
+            symbol_dot = "."
+
+        print(f"from : {file.path} to : {temp_folder + folder_app_name + normalize_name + symbol_dot + file.app}")
+        shutil.copy(file.fullname, temp_folder + folder_app_name + normalize_name + symbol_dot + file.app)
+
+    #delete origin files
+    for file in folder:
+        os.remove(file)
+
+    #copy in origin folder
+    for _folder in os.listdir(temp_folder):
+        shutil.copytree(temp_folder + "\\" + _folder, folder + "\\" + _folder)
 
 
 # Press the green button in the gutter to run the script.
