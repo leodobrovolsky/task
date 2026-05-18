b566735fe5eaa71f17cdc431714590c455edf612
Добровольский Леонид <l_dobrovolsky@viyar.com.ua>
2026-05-18 18:29:50 +0300

    task 2 begin

---
+++ task2.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+_users = {"name": "Bill Gates", "birthday": datetime(1955, 10, 28).date()}
+
+
+def get_birthdays_per_week(users):
+    now = datetime.today()
+
+
+
+
+
+get_birthdays_per_week(_users)
